4c020a0b7f1acae54ac8c437b7e2525fcd955c3e
slashhuang <627284482@qq.com>
2016-12-13 18:52:24 +0800

    add 2nd page to repo

--- app/src/main/java/com/slashhuang/myfirstapplication/RnTest.java
+++ app/src/main/java/com/slashhuang/myfirstapplication/RnTest.java
@@ -6,15 +6,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ScrollView;
+
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.shell.MainReactPackage;
 
 public class RnTest extends AppCompatActivity {
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rn_test);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +30,16 @@ public class RnTest extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        mReactRootView = (ReactRootView) findViewById(R.id.rn_container);
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModuleName("index.android")
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+        mReactRootView.startReactApplication(mReactInstanceManager, "TestRn1", null);
     }
 
 }
--- index.android.js
+++ index.android.js
@@ -19,12 +19,23 @@ class MyAwesomeApp extends React.Component {
     )
   }
 }
+class MyAwesomeApp1 extends React.Component {
+  render() {
+    return (
+      <View >
+        <Text style={styles.hello}>这是第二个页面</Text>
+        <Text style={styles.hello}>博客地址:github.com/slashhuang</Text>
+      </View>
+    )
+  }
+}
 var styles = StyleSheet.create({
   hello: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 100,
   },
 });
 
 AppRegistry.registerComponent('TestRn', () => MyAwesomeApp);
+AppRegistry.registerComponent('TestRn1', () => MyAwesomeApp1);
